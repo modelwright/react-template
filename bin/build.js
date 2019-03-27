b26575b43e14e4bea7fb59bf71841ea0e3d65dbc
unknown <uittle@163.com>
2019-03-27 15:24:35 +0800

    生产环境

---
+++ bin/build.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack')
+const chalk   = require('chalk')
+const config  = require('../config/webpack.pro.config')
+
+webpack(config).run((err, stats) => {
+
+    process.stdout.write(stats.toString({
+        colors       : true,
+        modules      : false,
+        children     : false,
+        chunks       : false,
+        chunkModules : false,
+        timings      : false
+    }) + '\n\n')
+
+    if (err || stats.hasErrors()) {
+        console.log(chalk.red('  Webpack打包错误！\n'))
+    } else {
+        console.log('Webpack打包成功！ cs ../dist. \n')
+    }
+
+})
